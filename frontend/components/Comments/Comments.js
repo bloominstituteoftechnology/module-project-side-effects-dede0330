9237ee88abe1bc4388bc16c3c94881a3a33d76e4
dede0330 <deandre.bloomfield@gmail.com>
2024-06-26 23:52:15 -0700

    complete
    complete

--- frontend/components/Comments/Comments.js
+++ frontend/components/Comments/Comments.js
@@ -8,7 +8,10 @@ const Comments = props => {
   return (
     <div className='comments-wrapper'>
       {/* map through the comments prop and render a Comment for every piece of data */}
-    </div>
+      {
+      comments.map(comm => <Comment comment ={comm} key={comm.id} />)
+      }      
+            </div>
   );
 };
 
